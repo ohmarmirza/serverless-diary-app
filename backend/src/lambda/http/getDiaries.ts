50376862e6f630c9e06dcc0534077040e903ac86
Omar Mirza <omar_mirza@mckinsey.com>
2022-05-12 13:30:23 -0400

    fixing issue witl multople path with api gateway

--- backend/src/lambda/http/getDiaries.ts
+++ backend/src/lambda/http/getDiaries.ts
@@ -4,11 +4,15 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-import { getDiariesForUser } from '../../businessLogic/diaries'
+import { getDiariesForUser, getDiariesForUserByDate } from '../../businessLogic/diaries'
 import { getUserId } from '../utils';
 import { createLogger } from '../../utils/logger'
 
 const logger = createLogger('getDiaries')
+
+function isDate(dateStr) {
+  return !isNaN(new Date(dateStr).getDate());
+}
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
@@ -16,7 +20,15 @@ export const handler = middy(
     logger.info('Handler - Processing getDiaries event', { event })
 
     const userId = getUserId(event)
-    const diaries = await getDiariesForUser(userId);
+    let diaries = []
+    const date = event.queryStringParameters.date
+
+    if (isDate(date)) {
+      diaries = await getDiariesForUserByDate(userId, new Date(date));
+    }
+    else {
+      diaries = await getDiariesForUser(userId);
+    }
 
     return {
       statusCode: 200,
